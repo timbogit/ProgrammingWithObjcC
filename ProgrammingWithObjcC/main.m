b009b362054fe8444c07934b2d4cd3814bcbee8b
Tim Schmelmer <tim.schmelmer@gmail.com>
2015-02-26 19:29:41 -0700

    Initial Commit

---
+++ ProgrammingWithObjcC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ProgrammingWithObjcC
+//
+//  Created by Tim Schmelmer on 26.02.15.
+//  Copyright (c) 2015 Tim Schmelmer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
